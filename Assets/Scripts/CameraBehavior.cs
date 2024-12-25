84fa89d31473ccf98897c2336ba2d2b2a413d26b
Yilmaz Tarik Ipekci <ytarik.ipekci@gmail.com>
2024-12-25 16:45:18 +0300

    Added -> Camera Follow

---
+++ Assets/Scripts/CameraBehavior.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class CameraBehavior : MonoBehaviour
+{
+    public Transform target;
+    private Vector3 offset;
+
+    private void Start()
+    {
+        offset = transform.position - target.position;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 newPosition = new Vector3(target.position.x, transform.position.y, offset.z + target.position.z);
+        transform.position = Vector3.Lerp(transform.position, newPosition, 10 * Time.deltaTime);
+    }
+}
