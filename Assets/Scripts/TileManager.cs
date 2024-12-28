08b8aa52d41ab08ee21433b8b9f2c3167a492485
Yilmaz Tarik Ipekci <ytarik.ipekci@gmail.com>
2024-12-28 17:22:40 +0300

    Added -> Infinite tile management

--- Assets/Scripts/TileManager.cs
+++ Assets/Scripts/TileManager.cs
@@ -1,23 +1,51 @@
+using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class TileManager : MonoBehaviour
 {
     public Tile[] tiles;
     private float zSpawn;
     private float tileLength;
+    private int numberOfTiles;
+    private List<Tile> activeTiles = new List<Tile>();
+    public Transform playerTransform;
 
     private void Start()
     {
-        SpawnTile(0);
-        SpawnTile(1);
-        SpawnTile(2);
+        numberOfTiles = tiles.Length;
+        for (int i = 0; i < numberOfTiles; i++)
+        {
+            var randomTileIndex = Random.Range(0, tiles.Length);
+            if (i == 0 && randomTileIndex == 1)
+            {
+                randomTileIndex = 0;
+            }
+            SpawnTile(randomTileIndex);
+        }
+    }
+
+    private void Update()
+    {
+        if (playerTransform.position.z - (2 * tileLength) > zSpawn - (numberOfTiles * tileLength))
+        {
+            SpawnTile(Random.Range(0, tiles.Length));
+            DestroyTile();
+        }
     }
 
     private void SpawnTile(int tileIndex)
     {
-        Instantiate(tiles[tileIndex], transform.forward * zSpawn, Quaternion.identity);
+        var newTile = Instantiate(tiles[tileIndex], transform.forward * zSpawn, Quaternion.identity);
+        activeTiles.Add(newTile);
         var meshRenderer = tiles[tileIndex].MeshRenderer;
         tileLength = meshRenderer.bounds.size.z - 1.5f;
         zSpawn += tileLength;
+    }
+
+    private void DestroyTile()
+    {
+        Destroy(activeTiles[0].gameObject);
+        activeTiles.RemoveAt(0);
     }
 }
